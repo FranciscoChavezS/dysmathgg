d4f45c5a02bfab06a6ace99b5cfe7ab300c3f5eb
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-02-24 14:38:14 -0600

    Habilitar botones de Anterior y Siguiente

--- app/Http/Livewire/CourseStatus.php
+++ app/Http/Livewire/CourseStatus.php
@@ -4,19 +4,27 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Course;
+use App\Models\Lesson;
 
 class CourseStatus extends Component
 {
-    
+    //Declarar variables para mostrar en la vista
     public $course;
     public $current;
 
     public function mount(Course $course){
         $this->course = $course;
 
-        foreach($course-> lessons as $lesson){
+        //Recorremos el arreglo para que busque las lecciones del curso y se detenga en la que no está completada
+        foreach($course-> lessons as $lesson){ 
             if(!$lesson->completed){
                 $this->current = $lesson;
+
+                /* //indice - Recuperar colección para verificar a todas las lecciones que le corresponden a un curso
+                $this->index = $course->lessons->search($lesson);
+
+                $this->previous = $course->lessons[$this->index - 1];
+                $this->next = $course->lessons[$this->index + 1]; */
                 break;
             }
         }
@@ -26,4 +34,29 @@ class CourseStatus extends Component
     {
         return view('livewire.course-status');
     }
+
+    //Remplazar los valores de current
+    public function changeLesson(Lesson $lesson){
+        $this->current = $lesson;        
+       
+    }
+
+    public function getIndexProperty(){
+        return $this->course->lessons->pluck('id')->search($this->current->id);
+    }
+    public function getPreviousProperty(){
+        if($this->index == 0){
+            return null;
+        }else{
+            return $this->course->lessons[$this->index - 1];
+        }
+    }
+    public function getNextProperty(){
+        if($this->index == $this->course->lessons->count() - 1){
+            return null;
+        }else{
+            return $this->course->lessons[$this->index + 1];
+        }
+    }
+
 }
