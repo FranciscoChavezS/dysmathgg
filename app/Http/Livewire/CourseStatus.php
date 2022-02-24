264f4fb949657d1d7bbf8dc3a1e80275eb2e0325
FranciscoChavezS <francisco.csalado@alumnos.udg.mx>
2022-02-24 17:18:04 -0600

    Marcar Actividad como completada y guardar progreso en la BD

--- app/Http/Livewire/CourseStatus.php
+++ app/Http/Livewire/CourseStatus.php
@@ -15,10 +15,12 @@ class CourseStatus extends Component
     public function mount(Course $course){
         $this->course = $course;
 
-        //Recorremos el arreglo para que busque las lecciones del curso y se detenga en la que no está completada
+        //Recorremos todas las lecciones que tenga el curso y que se almacenen en $lesson
         foreach($course-> lessons as $lesson){ 
+            //Si existe una lección que no este completada
             if(!$lesson->completed){
                 $this->current = $lesson;
+
 
                 /* //indice - Recuperar colección para verificar a todas las lecciones que le corresponden a un curso
                 $this->index = $course->lessons->search($lesson);
@@ -27,6 +29,11 @@ class CourseStatus extends Component
                 $this->next = $course->lessons[$this->index + 1]; */
                 break;
             }
+            //Si todas las lecciones están marcadas como terminadas se asigna la última lección 
+            if(!$this->current){
+                $this->current = $course->lessons->last();
+
+            }
         }
     }
 
@@ -34,13 +41,31 @@ class CourseStatus extends Component
     {
         return view('livewire.course-status');
     }
-
+    /* MÉTODOS */
     //Remplazar los valores de current
     public function changeLesson(Lesson $lesson){
         $this->current = $lesson;        
        
     }
 
+    public function completed(){
+        //Si la unidad esta marcada 
+        if($this->current->completed){
+            //Eliminar registro
+            $this->current->users()->detach(auth()->user()->id);
+        }else{
+            //Agregar Registro
+            $this->current->users()->attach(auth()->user()->id);
+
+        }
+
+        //Recuperar datos para actualizar la información
+        $this->current = Lesson::find($this->current->id);
+        $this->course = Course::find($this->course->id);
+    }
+
+    /* PROPIEDADES COMPUTADAS*/
+    
     public function getIndexProperty(){
         return $this->course->lessons->pluck('id')->search($this->current->id);
     }
@@ -57,6 +82,22 @@ class CourseStatus extends Component
         }else{
             return $this->course->lessons[$this->index + 1];
         }
+    }
+    //Registrar progreso del estudiante
+    public function getAdvanceProperty(){
+      $i = 0;
+
+      foreach($this->course->lessons as $lesson){
+          //Si el usuario marca la actividad completada entra a la condición
+          if($lesson->completed){
+            $i++;
+          }
+        }
+
+        $advance = ($i * 100)/($this->course->lessons->count());
+
+        //Mostrar cantidad de decimales
+        return round($advance, 1);
     }
 
 }
